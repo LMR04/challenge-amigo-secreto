f0d5671f1c6939b94bf977befa8a4889fafa70f6
LMR04 <102443174+LMR04@users.noreply.github.com>
2025-08-11 20:36:18 -0500

    add js code

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    if (nombre.trim() === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    } else{
+        amigos.push(nombre);
+        document.getElementById('amigo').value = '';
+        mostrarAmigos();
+    }
+}
+
+function mostrarAmigos() {
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let amigo = amigos[i];
+        let listaHTML = document.createElement('li');
+        listaHTML.textContent = amigo;
+        lista.appendChild(listaHTML);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear.');
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let resultadoHTML = document.querySelector('#resultado');
+    document.getElementById('listaAmigos').innerHTML = '';
+    resultadoHTML.innerHTML = amigos[indiceAleatorio];
+}
